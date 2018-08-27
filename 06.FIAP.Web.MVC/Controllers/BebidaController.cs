f774aff1a1ee98710350d32a74d6dc9bf9b90699
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-27 20:52:04 -0300

    CRUD C#

--- 06.FIAP.Web.MVC/Controllers/BebidaController.cs
+++ 06.FIAP.Web.MVC/Controllers/BebidaController.cs
@@ -11,6 +11,29 @@ namespace _06.FIAP.Web.MVC.Controllers
     public class BebidaController : Controller
     {
         private BotecoContext _context = new BotecoContext();
+
+        [HttpPost]
+        public ActionResult Atualizar(Bebida bebida)
+        {
+            //Atualiza o banco de dados
+            _context.Entry(bebida).State = System.Data.Entity.EntityState.Modified;
+            _context.SaveChanges();
+            //Mensagem
+            TempData["msg"] = "Bebida atualizada!";
+            //Redirecionar
+            return RedirectToAction("Listar");
+        }
+
+
+        [HttpGet]
+        public ActionResult Atualizar(int id)
+        {
+            //Busca a bebida no DB
+            var bebida = _context.Bebidas.Find(id);
+            //Retorna a view com o objeto bebida
+            return View(bebida);
+        }
+
         [HttpGet]
         public ActionResult Index()
         {
@@ -22,7 +45,15 @@ namespace _06.FIAP.Web.MVC.Controllers
         {
             _context.Bebidas.Add(bebida);
             _context.SaveChanges();
+            TempData["msg"] = "Bebida cadastrada!";
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            //Recupera todas as bebidas e envia para a tela
+            return View(_context.Bebidas.ToList());
         }
     }
 }
